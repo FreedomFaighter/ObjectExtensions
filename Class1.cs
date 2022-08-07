8a2aa44d04dd83b42be07afab74f8395a12a5770
mathgrad-admin <mathgrad-admin@BLACKBETTY>
2022-08-06 23:40:47 -0400

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObjectExtensions
+{
+    static public class Extensions
+    {
+        static public Boolean IsNull(this Object o)
+        {
+            return o == null;
+        }
+    }
+}
