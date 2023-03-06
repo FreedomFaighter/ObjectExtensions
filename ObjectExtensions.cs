5e163a3463eb263d138556623469000ade735f98
John H. Freeman <johnherschelfreeman2013@gmail.com>
2023-03-06 18:57:50 -0500

    Update ObjectExtensions.cs

--- ObjectExtensions.cs
+++ ObjectExtensions.cs
@@ -1,5 +1,12 @@
 ﻿using System;
 
+[assembly: AssemblyTitle("ObjectExtensions")]
+[assembly: AssemblyDescription("Simple extension to check if your object is a null value in .NET")]
+[assembly: AssemblyCompany("John H. Freeman")]
+[assembly: AssemblyVersion("0.1")]
+[assembly: AssemblyCopyright("2012")]
+[assembly: AssemblyTrademark("brainkernel")]
+
 namespace ObjectExtensions
 {
     static public class ObjectExtensions
