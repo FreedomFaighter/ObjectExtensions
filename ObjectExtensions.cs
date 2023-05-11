99cb06e3a1dfa1a1c40293aecf88f1fcd5bb0b9c
FreedomFaighter <johnherschelfreeman2013@gmail.com>
2023-05-11 14:44:33 -0400

    Update ObjectExtensions.cs

--- ObjectExtensions.cs
+++ ObjectExtensions.cs
@@ -3,9 +3,7 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("ObjectExtensions")]
 [assembly: AssemblyDescription("Simple extension to check if your object is a null value in .NET")]
-[assembly: AssemblyCompany("John H. Freeman")]
-[assembly: AssemblyVersion("0.1")]
-[assembly: AssemblyTrademark("brainkernel")]
+[assembly: AssemblyVersion("0.0.0.1")]
 
 namespace ObjectExtensions
 {
